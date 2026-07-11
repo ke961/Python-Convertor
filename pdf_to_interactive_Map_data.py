b6c91e26a955a25d8e3b868f98a7ee7b8f990e5e
Abila Khan <khankeya961@gmail.com>
2026-07-12 00:37:29 +0600

    Update pdf_to_interactive_Map_data.py

--- pdf_to_interactive_Map_data.py
+++ pdf_to_interactive_Map_data.py
@@ -2,6 +2,7 @@ from pypdf import PdfReader
 from geopy.geocoders import Nominatim
 import re
 import time 
+import folium
 
 
 
@@ -24,4 +25,33 @@ places= re.findall(r'\b[A-Z][a-z]+(?:\s[A-Z][a-z]+)*\b', text)
 places= list(set(places))  # Remove duplicates
 print ("Places extracted from PDF:")
 print(places)
+
+
+#Geocoder
+geolocation=Nominatim(user_agent="korea_map")
+
+#Creat Map centered on south korea
+
+m = folium.Map (location=[36.5, 127.5], zoom_start=7)
+
+for place in places:
+    try:
+        location= geolocation.geocode(f"{place}, South Korea")
+        
+
+
+        if location:
+            print(f"Geocoded {place}: {location.latitude}, {location.longitude}")
+            folium.Marker([location.latitude, location.longitude], tooltip=place, popup=place).add_to(m)
+
+
+
+        time.sleep(1)  # To avoid hitting the rate limit of the geocoding service
+    except Exception as e:
+        print(f"Error geocoding {place}: {e}")
+
+
+
+m.save("korea_places_map.html")
+print("Map saved as korea_places_map.html")
 
