50b3fdfaf9738d94ab2d3e610ad34e73d090c8f3
Abila Khan <khankeya961@gmail.com>
2026-07-12 00:19:56 +0600

    Update pdf_to_interactive_Map_data.py

--- pdf_to_interactive_Map_data.py
+++ pdf_to_interactive_Map_data.py
@@ -2,8 +2,11 @@ from pypdf import PdfReader
 
 import re
 
-reader = PdfReader("file.pdf")
+reader = PdfReader("korea_places.pdf")
 text= reader.pages[0].extract_text()
 places= re.findall(r'\b[A-Z][a-z]+(?:\s[A-Z][a-z]+)*\b', text)
 
 print(places)
+
+for p in set(places):
+    print({"location": p})
